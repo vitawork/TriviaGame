1587f1ddb7322ebeb7d48f3d985972771f70d0ab
Esteban Morell <estebanmorellb@gmail.com>
2019-07-19 12:06:33 -0500

    logic finished

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,9 +1,13 @@
 $(document).ready(function() {
-  var timer = 0;
+  var timer = 60;
   var right = 0;
   var wrong = 0;
   var tout = 0;
-  var actualqindex = 0;
+  var actualqindex = -1;
+
+  var next_timeout;
+  var question_timeout;
+  var intervalId;
 
   var questions = [
     {
@@ -24,22 +28,11 @@ $(document).ready(function() {
   ];
 
   function reset() {
-    $("#divcentral1").css("display", "block");
-    $("#divcentral2").css("display", "none");
-    $("#divcentral3").css("display", "none");
-
-    $("#timerrow h4").text("00:00");
-    $("#winrow h4").text("0");
-    $("#looserow h4").text("0");
-    $("#timeoutrow h4").text("0");
-
-    timer = 0;
     right = 0;
     wrong = 0;
     tout = 0;
-    actualqindex = 0;
-
-    show_question_answers(actualqindex);
+    actualqindex = -1;
+    next();
   }
 
   function show_question_answers(index) {
@@ -52,7 +45,9 @@ $(document).ready(function() {
   reset(); ///////reseating to start
 
   function right_wrong_timeout_answer(rwt) {
-    ////////////////////////////////////////////////////////////////////
+    clearTimeout(question_timeout);
+    clearInterval(intervalId);
+
     if (rwt === "Right Answer") {
       right++;
       $("#winrow h4").text(right);
@@ -66,22 +61,80 @@ $(document).ready(function() {
       }
     }
 
+    $("#divcentral1").css("display", "none");
+    $("#divcentral2").css("display", "block");
+    $("#divcentral3").css("display", "none");
+    $("#rwt").text(rwt);
+    $("#answer").text(
+      questions[actualqindex].answers[questions[actualqindex].ansposition]
+    );
+    if (actualqindex === questions.length - 1) {
+      $(".next").text("Finish");
+    }
+    next_timeout = setTimeout(function() {
+      if (actualqindex === questions.length - 1) {
+        $("#divcentral1").css("display", "none");
+        $("#divcentral2").css("display", "none");
+        $("#divcentral3").css("display", "block");
+      } else {
+        next();
+      }
+    }, 7000);
+  }
+
+  function next() {
     if (actualqindex === questions.length - 1) {
       $("#divcentral1").css("display", "none");
       $("#divcentral2").css("display", "none");
       $("#divcentral3").css("display", "block");
     } else {
-      $("#divcentral1").css("display", "none");
-      $("#divcentral2").css("display", "block");
+      $(".next").text("Next");
+      actualqindex++;
+      timer = 60;
+      $("#divcentral1").css("display", "block");
+      $("#divcentral2").css("display", "none");
       $("#divcentral3").css("display", "none");
-      $("#rwt").text(rwt);
-      $("#answer").text(
-        questions[actualqindex].answers[questions[actualqindex].ansposition]
-      );
-      //////parar y actualizar timer
+
+      $("#timerrow h4").text("00:00");
+      $("#winrow h4").text(right);
+      $("#looserow h4").text(wrong);
+      $("#timeoutrow h4").text(tout);
+
+      show_question_answers(actualqindex);
+
+      intervalId = setInterval(count, 1000);
+
+      question_timeout = setTimeout(function() {
+        clearInterval(intervalId);
+        right_wrong_timeout_answer("Time Out");
+      }, 1000 * timer);
     }
   }
 
+  function count() {
+    timer--;
+    var converted = timeConverter(timer);
+    $("#timerrow h4").text(converted);
+  }
+
+  function timeConverter(t) {
+    var minutes = Math.floor(t / 60);
+    var seconds = t - minutes * 60;
+
+    if (seconds < 10) {
+      seconds = "0" + seconds;
+    }
+
+    if (minutes === 0) {
+      minutes = "00";
+    } else if (minutes < 10) {
+      minutes = "0" + minutes;
+    }
+
+    return minutes + ":" + seconds;
+  }
+
+  // answers buttons////////////
   $("#answer1").click(function() {
     if (questions[actualqindex].ansposition === 0) {
       right_wrong_timeout_answer("Right Answer");
@@ -105,19 +158,10 @@ $(document).ready(function() {
     }
   });
 
+  // button next///////////
   $(".next").click(function() {
-    actualqindex++;
-    timer = 0;
-    $("#divcentral1").css("display", "block");
-    $("#divcentral2").css("display", "none");
-    $("#divcentral3").css("display", "none");
-
-    // $("#timerrow h4").text("00:00");   arreglar timer  ////////////
-    $("#winrow h4").text(right);
-    $("#looserow h4").text(wrong);
-    $("#timeoutrow h4").text(tout);
-
-    show_question_answers(actualqindex);
+    next();
+    clearTimeout(next_timeout);
   });
 
   $(".start").click(function() {
